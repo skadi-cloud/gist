6a74a69efac673f84d119e4994cb7eed622c44b7
Kolja Dummann <kolja@hey.com>
2021-09-25 05:59:26 +0200

    add js build

---
+++ js/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.github.node-gradle.node") version "3.1.1"
+}
+
+val runWebpack by tasks.creating(com.github.gradle.node.npm.task.NpxTask::class) {
+    dependsOn(tasks.getByName("yarn_install"))
+    command.set("webpack")
+}
+
+tasks.create("build") {
+    dependsOn(runWebpack)
+} 
