59a7d69611da81eb3855d256599ef6d7be37ae44
Kolja Dummann <kolja@hey.com>
2021-10-09 22:13:27 +0200

    convert main build script to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.5.31" apply false
+    id("org.jetbrains.intellij") version "1.2.0" apply false
+}
+
+subprojects {
+    group = "cloud.skadi.gist"
+}
